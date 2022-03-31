ad32a9bd46f525db12e8a359de1b9c7e75fdf650
adrianongit <adriansantosrollim@gmail.com>
2022-03-30 21:27:52 -0300

    Demo 0.3

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 int main(){
 
     int horas = 0;
-    int mnts = 295;
+    int mnts = 180;
 
-    while(mnts > 10){
-        mnts = mnts - 59;
-        horas++;
-        printf("Agora sao %dh e %imin\n", horas, mnts);
+    while(mnts > 59){
+        if(mnts > 0){
+            mnts = mnts - 59;
+            horas++;
+            printf("Agora sao %dh e %imin\n", horas, mnts);
+        }else{
+            mnts = mnts * (-1);
+        }
     }
     return 0;
 }
