4ea1697f5c5bea622bc3c8984c8c3c1f2391ad4b
adrianongit <adriansantosrollim@gmail.com>
2022-03-31 15:14:28 -0300

    Demo 0.4

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sobre este projeto
+
+Um algoritmo que converte minutos em horas e horas em dias
+
+# Tecnologias utilizadas
+
+- Linguagem de programação C
--- main.c
+++ main.c
@@ -4,16 +4,22 @@
 
 int main(){
 
+    int dias = 0;
     int horas = 0;
-    int mnts = 180;
+    int mnts = 18000;
 
     while(mnts > 59){
         if(mnts > 0){
             mnts = mnts - 59;
             horas++;
-            printf("Agora sao %dh e %imin\n", horas, mnts);
+            printf("Agora sao %dh e %imin, %i dias\n", horas, mnts, dias);
         }else{
             mnts = mnts * (-1);
+        }
+
+        if(horas > 23){
+            horas = 0;
+            dias++;
         }
     }
     return 0;
